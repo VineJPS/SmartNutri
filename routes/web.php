0da6c26df1ec9aef22dcaee1e835ae4e66a98020
VineJPS <vinicius108sousa@gmail.com>
2025-08-15 15:32:04 -0300

    Subindo os arquivos do Laravel

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\LogAcessoMiddleware;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+Route::get('/teste', [App\Http\Controllers\Principal::class, 'teste']);
+
+Route::get('/conectar', [App\Http\Controllers\Usuario::class, 'conectar']);
+Route::get('/desconectar', [App\Http\Controllers\Usuario::class, 'desconectar']);
+
